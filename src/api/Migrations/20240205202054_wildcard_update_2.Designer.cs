79157691e2ed88be6de57d043812a2bb369c8876
BBITWestin <twestin@bbilogistics.com>
2024-02-05 15:21:10 -0500

    wildcard attempt 139685 expecting wildcard 2 after adding migration

--- src/api/Migrations/20240205202054_wildcard_update_2.Designer.cs
+++ src/api/Migrations/20240205202054_wildcard_update_2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Todo.Api.Migrations
 {
     [DbContext(typeof(TodoDb))]
-    partial class TodoDbModelSnapshot : ModelSnapshot
+    [Migration("20240205202054_wildcard_update_2")]
+    partial class wildcard_update_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
